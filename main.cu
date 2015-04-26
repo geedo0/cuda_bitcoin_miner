8a2d33a818e72c0bfacd50537c07220cb7e30fd5
Gerardo Ravago <gcravago@hpcl-18.ad.bu.edu>
2015-04-26 16:35:39 -0400

    Adds dummy kernel call

--- main.cu
+++ main.cu
@@ -3,18 +3,25 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "cuPrintf.cu"
+#include "cuPrintf.cuh"
 #include "utils.cuh"
 
 #include "test.h"
+
+__global__ void kernel_sha256d(unsigned char *data, Nonce_result *nr);
 
 int main(int argc, char **argv) {
 	int i;
 	unsigned char *data = test_block;
 	size_t datasz = 76;
 
+
+	cudaPrintfInit();
+
 	//Initialize Nonce Result to see if we solved the block
-	Nonce_result nr;
-	initialize_nonce_result(&nr);
+	Nonce_result h_nr;
+	initialize_nonce_result(&h_nr);
 	
 	//Allocate space on Global Memory
 	unsigned char *d_block_header;
@@ -24,24 +31,37 @@ int main(int argc, char **argv) {
 
 	//Copy data structures to device
 	CUDA_SAFE_CALL(cudaMemcpy(d_block_header, data, datasz, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_nr, (void *) &nr, sizeof(Nonce_result), cudaMemcpyHostToDevice));
+	CUDA_SAFE_CALL(cudaMemcpy(d_nr, (void *) &h_nr, sizeof(Nonce_result), cudaMemcpyHostToDevice));
+
+	//Launch kernel
+	dim3 DimBlock(1,1);
+	dim3 DimGrid(1,1);
+	kernel_sha256d<<<DimGrid, DimBlock>>>(d_block_header, d_nr);
 
 	//Copy data back to host
-	Nonce_result nr_from_device;
-	CUDA_SAFE_CALL(cudaMemcpy((void *) &nr_from_device, d_nr, sizeof(Nonce_result), cudaMemcpyDeviceToHost));
+	CUDA_SAFE_CALL(cudaMemcpy((void *) &h_nr, d_nr, sizeof(Nonce_result), cudaMemcpyDeviceToHost));
 
-	if(nr_from_device.nonce_found) {
-		printf("Nonce found! %.8x\n", nr_from_device.nonce);
+	//Cuda Printf output
+	cudaDeviceSynchronize();
+	cudaPrintfDisplay(stdout, true);
+	cudaPrintfEnd();
+
+	//Free memory on device
+	CUDA_SAFE_CALL(cudaFree(d_block_header));
+	CUDA_SAFE_CALL(cudaFree(d_nr));
+
+	if(h_nr.nonce_found) {
+		printf("Nonce found! %.8x\n", h_nr.nonce);
 	}
 	else {
 		printf("Nonce not found :(\n");
 	}
-	
-
-	if(nr_from_device.nonce == nr.nonce)
-		printf("Copy passed\n");
-	else
-		printf("Copy failed %d %d", nr_from_device.nonce, nr.nonce);
 
 	return 0;
+}
+
+__global__ void kernel_sha256d(unsigned char *data, Nonce_result *nr) {
+	cuPrintf("Hello nonce %x\n", nr->nonce);
+	nr->nonce = 0xdeadbeef;
+	nr->nonce_found = true;
 }
--- makefile
+++ makefile
@@ -4,4 +4,4 @@ CC=nvcc
 all: $(OBJECTS)
 
 gpu_miner: main.cu utils.cu
-	$(CC) -O1 -v -lrt -lm -arch=sm_20 -o $@ $^
+	$(CC) -O1 -lrt -lm -arch=sm_20 -o $@ $^
